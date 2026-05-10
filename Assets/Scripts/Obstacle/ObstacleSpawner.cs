75cb9da1dfab62853d55a750ac9fdd4bb59d6f48
9ga0 <koo050803@gmail.com>
2026-05-11 07:08:39 +0900

    feat: 구현된 기능을 MainScene에 연결
    - Obstacle.cs와 ObstacleSpawner.cs의 주석 수정
    - ObstacleSpawner의 소환위치 y값 0f로 수정
    - MainScene에 있는 ObstacleSpawner에 스크립트 컴포넌트 넣고 프리팹 넣기

--- Assets/Scripts/Obstacle/ObstacleSpawner.cs
+++ Assets/Scripts/Obstacle/ObstacleSpawner.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 /// 기능
 /// : 일정한 시간마다 카메라의 오른쪽 뒤에서 장애물을 소환한다.
 /// 
-/// 추후 변경 계획
-/// : 현재 -20f로 고정값인 생성할 위치의 y값(spawnY)은 ground를 참고하여 값을 지정할 예정.
+/// 추후 개선 계획
+/// : 현재 고정값 0f인 생성위치의 y값(spawnY)은 원하는 위치에 따라 값을 지정.
 /// : 현재 오브젝트가 생성되는 시간이 일정한데, 불규칙적이도록 수정해야함.
-/// 
-/// 초기 설정
-/// 1. hierarchy에 이 스크립트를 추가하기
-/// 2. 프리팹 칸에 Obstacle이라는 Prefab를 넣기
-/// 3. 메인카메라가 없다면, hierarchy에 카메라오브젝트 생성 후, 카메라의 태그를 MainCamera로 고치가
+/// : 다양한 종류의 장애물 오브젝트를 구현해야함.
 /// </summary>
 
 public class ObstacleSpawner : MonoBehaviour
@@ -19,7 +15,7 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject obstaclePrefab; // 생성할 장애물 프리팹. 
 
     private float spawnX; //생성할 위치의 x값
-    private float spawnY = -20f; //생성할 위치의 y값
+    private float spawnY = 0f; //생성할 위치의 y값
     private float startDelay = 2f; // 첫 생성 대기 시간
     private float repeatRate = 3f; // 반복 간격 (3초)
 
