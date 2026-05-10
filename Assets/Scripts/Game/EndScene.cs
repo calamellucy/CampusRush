72607a397e479b077cf2bcaf3307eefd0e59b4b0
calamellucy <sua050526@naver.com>
2026-05-10 20:22:36 +0900

    docs: 코드 주석 추가
    - StartScene.cs, EndScene.cs 파일 주석 추가

--- Assets/Scripts/Game/EndScene.cs
+++ Assets/Scripts/Game/EndScene.cs
@@ -3,13 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class EndScene : MonoBehaviour
 {
+    // [수아]
+
     public void OnClickRestart()
     {
         SceneManager.LoadScene("MainScene");
+        // Restart 버튼 클릭 시 MainScene으로 이동
     }
 
     public void OnClickMain()
     {
         SceneManager.LoadScene("StartScene");
+        // Main 버튼 클릭 시 StartScene으로 이동
     }
 }
--- Assets/Scripts/Game/StartScene.cs
+++ Assets/Scripts/Game/StartScene.cs
@@ -3,13 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour
 {
+    // [수아]
     public void OnClickStart()
     {
         SceneManager.LoadScene("MainScene");
+        // Start 버튼 클릭 시 MainScene으로 이동
     }
 
     public void OnClickExit()
     {
         Application.Quit();
+        // Exit 버튼 클릭 시 게임 종료
     }
 }
