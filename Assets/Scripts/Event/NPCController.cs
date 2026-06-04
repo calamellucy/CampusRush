fa21dd9f2c5b7171f9a2d80aa4bc132d33c7b52b
9ga0 <koo050803@gmail.com>
2026-06-04 13:57:43 +0900

    feat: NPC의 이동속도도 장애물 속도를 따라 점진적으로 향상되도록 수정

--- Assets/Scripts/Event/NPCController.cs
+++ Assets/Scripts/Event/NPCController.cs
@@ -11,6 +11,9 @@ public class NPCController : MonoBehaviour
     public void Init(NPCEventManager manager)
     {
         eventManager = manager;
+        // [가영] 생성 시점의 현재 속도를 가져옴 (도로/장애물과 동일한 속도)
+        moveSpeed = ObstacleSpawner.currentSpeed;
+
     }
 
     void Start()
@@ -18,10 +21,10 @@ public class NPCController : MonoBehaviour
         escapeX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x - 2f;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        // [채원] 왼쪽 방향으로 천천히 이동
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        // [채원] 왼쪽 방향으로 천천히 이동      //[가영] 장애물과 동일한 공용 속도로 이동
+        transform.Translate(Vector2.left * moveSpeed * Time.fixedDeltaTime);
 
         // [채원] 화면 왼쪽 끝을 벗어났는지 체크 (화면 좌표 기준)
         if (transform.position.x < escapeX && !isTriggered)
